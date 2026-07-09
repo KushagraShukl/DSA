db67322e95fb9ddf5e36e4b3bd5f7130c2aaab4a
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-07-09 15:45:23 +0530

    Time: 76 ms (45.99%) | Memory: 93.8 MB (72.86%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         int n = nums.size();
         unordered_set<int> window;
-        for(int i = 0; i<n; ++i){
+        for(int i = 0; i<n; i++){
             if(i>k){
                 window.erase(nums[i - k - 1]) ;
             }
