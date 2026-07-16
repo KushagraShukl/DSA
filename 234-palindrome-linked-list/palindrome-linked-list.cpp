770949697cbbcef0a33eeacae337acb9b846e1fe
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-07-16 22:37:41 +0530

    Time: 3 ms (59.34%) | Memory: 122 MB (38.30%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,16 +10,42 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> l;
+    ListNode* reverseList(ListNode* head) {
         ListNode* curr = head;
+        ListNode* p = NULL;
+        ListNode* n = NULL;
+
         while(curr){
-            l.push_back(curr->val);
-            curr = curr->next;
+            n = curr->next;
+            curr->next = p;
+            p = curr;
+            curr = n;
         }
-        for(int i = 0 ; i<l.size(); i++){
-            if (l[i] != l[l.size() - i - 1]) return false;
+        return p;
+    }
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next)
+            return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
+        ListNode* newhead = reverseList(slow->next);
+        ListNode* c1 = head;
+        ListNode* c2 = newhead;
+
+        while(c2){
+            if(c1->val != c2->val){
+                slow->next = reverseList(newhead);
+                return false;
+            }
+            c1 = c1->next;
+            c2 = c2->next;
+        }
+        slow->next = reverseList(newhead);
         return true;
+        
     }
 };
