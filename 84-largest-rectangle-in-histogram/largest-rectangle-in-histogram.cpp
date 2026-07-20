984c3afcce23e304931ccc37617e87c1ba402222
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-07-20 11:10:39 +0530

    Time: 25 ms (43.91%) | Memory: 85.8 MB (35.26%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -2,30 +2,33 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> left(n), right(n);
+        vector<int> right(n);
+        vector<int> left(n);
         stack<int> st;
-        for (int i = 0; i < n; i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
+        //left smaller value
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && heights[i]<=heights[st.top()]){
                 st.pop();
             }
-            left[i] = st.empty()? -1 : st.top();
+            if(st.empty()) left[i] = -1;
+            else left[i] = st.top();
             st.push(i);
         }
 
         while(!st.empty()){
             st.pop();
         }
-
-        for (int i = n-1; i >= 0; i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && heights[i]<=heights[st.top()]){
                 st.pop();
             }
-            right[i] = st.empty()? n : st.top();
+            if(st.empty()) right[i] = n;
+            else right[i] = st.top();
             st.push(i);
         }
-        int area, maxArea = 0;
+        int area,maxArea = 0;
         for(int i = 0; i<n; i++){
-            area = heights[i] * (right[i] - left[i] - 1);
+            area = heights[i] * (right[i] - left[i] -1);
             maxArea = max(area, maxArea);
         }
         return maxArea;
