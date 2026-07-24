9ab317d5b6ee5ce8915bd077cafbf50454c852c0
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-07-24 16:15:01 +0530

    Time: 4 ms (49.41%) | Memory: 121.7 MB (45.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,47 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* revLL(ListNode* head){
-        ListNode* prev = NULL;
-        ListNode* cur = head;
+    ListNode* revList(ListNode* head){
+        ListNode* p = NULL;
+        ListNode* curr = head;
         ListNode* n = NULL;
 
-        while(cur){
-            n = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = n;
+        while(curr){
+            n = curr->next;
+            curr->next = p;
+            p = curr;
+            curr = n;
         }
-        return prev;
-
+        return p;
     }
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next){
-            return true;
-        }
+        ListNode *fast = head, *slow = head;
 
-        ListNode *slow = head, *fast = head;
-        while(fast->next && fast->next->next) {
+        while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        ListNode* newHead = revLL(slow->next);
+        ListNode* newHead = revList(slow->next);
 
-        ListNode *c1 = head, *c2 = newHead;
+        ListNode* c1 = head;
+        ListNode* c2 = newHead;
 
         while(c2){
             if(c1->val != c2->val){
-                slow->next = revLL(newHead);
+                slow->next = revList(newHead);
                 return false;
             }
             c1 = c1->next;
             c2 = c2->next;
         }
-
-        slow->next = revLL(newHead);
+        slow->next = revList(newHead);
         return true;
 
-        
     }
 };
