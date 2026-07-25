57bd21a44284773a536099e6b35e2a9bb72228ad
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-07-25 21:57:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (10.11%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,18 +1,9 @@
 class Solution {
-public:
-    int maxProduct(int n) {
-        vector<int> arr;
-        while(n>0){
-            arr.push_back(n%10);
-            n = n/10;
-        }
-        int maxProd = 0, prod;
-        for(int i=0; i<arr.size(); i++){
-            for(int j = i+1; j<arr.size(); j++){
-                prod = arr[i] * arr[j];
-                maxProd = max(prod,maxProd);
-            }
-        }
-        return maxProd;
-    }
+ public:
+  int maxProduct(int n) {
+    string s = to_string(n);
+    ranges::sort(s);
+    const int sz = s.length();
+    return (s[sz - 1] - '0') * (s[sz - 2] - '0');
+  }
 };
