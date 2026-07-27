be2c7a309b204e44847ae58d34639f0bd27e0e7e
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-07-27 10:30:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (15.39%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    void findSubset(vector<int>& nums, int i,
-                    vector<int>& subset) {
-
-        // Base case: all elements have been considered
-        if (i == nums.size()) {
+    void allSubsets(int i, vector<int>& subset, vector<vector<int>>& ans, vector<int> nums){
+        if(i == nums.size()){
             ans.push_back(subset);
             return;
         }
 
-        // Include nums[i]
         subset.push_back(nums[i]);
-        findSubset(nums, i + 1, subset);
-
-        // Backtrack
+        allSubsets(i+1, subset, ans, nums);
         subset.pop_back();
-
-        // Exclude nums[i]
-        findSubset(nums, i + 1, subset);
+        allSubsets(i+1, subset, ans, nums);
     }
-
     vector<vector<int>> subsets(vector<int>& nums) {
-        
+        vector<vector<int>> ans;
         vector<int> subset;
-
-        findSubset(nums, 0, subset);
-
+        allSubsets(0, subset, ans, nums);
         return ans;
     }
 };
