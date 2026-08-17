e160f5bdae9d421bfc1e11a5ee046bc25d165af5
Kushagra Shukla <83111853+KushagraShukl@users.noreply.github.com>
2026-08-17 21:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (97.02%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> numSum1(n,0),numSum2(n,0);
-        for(int i = 1; i<n; i++){
-            numSum1[i] = numSum1[i-1] + nums[i-1];
-        }
-        for(int i = n-2; i>=0; i--){
-            numSum2[i] = numSum2[i+1] + nums[i+1];
-        }
-
+        int total = 0, left = 0, right = 0;
         for(int i = 0; i<n; i++){
-            if (numSum1[i] == numSum2[i]) return i;
+            total += nums[i]; 
+        }
+        for(int i = 0; i<n; i++){
+            right = total - left - nums[i];
+            if(right == left) return i;
+            left += nums[i];
         }
         return -1;
 
